77c67ba96daf125afe7391d39ef9db52e5423bca
NezlaAnnaisha <nezla.annaisha@gmail.com>
2026-03-08 14:49:05 -0600

    feat: added drag and hover function when mouse input interacts with objects.

---
+++ scripts/draggable.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var hovering = false
+var dragging = false
+var drag_offset = Vector2.ZERO
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("click") and hovering:
+		dragging = true
+		drag_offset = global_position - get_global_mouse_position()
+	
+	if Input.is_action_just_released("click"):
+		dragging = false
+	
+	if dragging:
+		global_position = get_global_mouse_position() + drag_offset
+
+func _on_mouse_entered() -> void:
+	hovering = true
+
+func _on_mouse_exited() -> void:
+	hovering = false
