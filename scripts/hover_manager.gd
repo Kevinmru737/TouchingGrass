dbdd3ba2a093b3186733247efa1b10f01ad8ea2f
NezlaAnnaisha <nezla.annaisha@gmail.com>
2026-03-08 16:31:40 -0600

    merge stash

--- scripts/hover_manager.gd
+++ scripts/hover_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var label: Label
+var current_hovered = null
 
 func _ready():
 	label = Label.new()
@@ -12,8 +13,10 @@ func _process(_delta):
 		label.global_position = get_viewport().get_mouse_position() + Vector2(16, -24)
 
 func on_hovered(interactable):
+	current_hovered = interactable
 	label.text = interactable.object_name
 	label.visible = true
 
 func on_unhovered(_interactable):
+	current_hovered = null
 	label.visible = false
--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
-@export var object_name: String = "Fish Tank"
+@export var object_name: String = "Object"
+@export var minigame_scene: PackedScene
 
 func _ready():
 	#mouse_entered.connect(_on_mouse_entered)
 	#mouse_exited.connect(_on_mouse_exited)
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if HoverManager.current_hovered == self:
+				if minigame_scene:
+					get_tree().current_scene.add_child(minigame_scene.instantiate())
 
 func _on_mouse_entered():
 	HoverManager.on_hovered(self)
