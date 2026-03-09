8b9ce28f7eaebd7d259dd4220db74a3be9079f43
Kevin <koh705@mtroyal.ca>
2026-03-08 18:51:09 -0600

    asd

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,11 +1,27 @@
 extends Area2D
 
+<<<<<<< HEAD
 @export var object_name: String = "Fish Tank"
 #@onready var backglow = $Backglow
 @onready var cursor = load("res://art/smallerfish.png")
 func _ready():
 	#backglow.modulate.a = 0  # Start invisible
+=======
+@export var object_name: String = "Object"
+@export var minigame_scene: PackedScene
+
+func _ready():
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+>>>>>>> c33a60ec45c0e48a55e742a463bf3c4e0c0ae3f3
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if HoverManager.current_hovered == self:
+				if minigame_scene:
+					get_tree().current_scene.add_child(minigame_scene.instantiate())
 
 func _on_mouse_entered():
 	HoverManager.on_hovered(self)
@@ -18,7 +34,11 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	HoverManager.on_unhovered(self)
+<<<<<<< HEAD
 	Input.set_custom_mouse_cursor(null)
 	#var tween = create_tween()
 	#tween.tween_property(backglow, "modulate:a", 0.0, 0.3)
 	#tween.parallel().tween_property(backglow, "scale", Vector2(1.0, 1.0), 0.3)
+=======
+	
+>>>>>>> c33a60ec45c0e48a55e742a463bf3c4e0c0ae3f3
