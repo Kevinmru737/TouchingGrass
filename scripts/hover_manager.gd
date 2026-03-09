f5a708d6c4f4d48fbf6eed30836d9533d4a4e39f
Kevin <koh705@mtroyal.ca>
2026-03-08 21:53:01 -0600

    added bed

--- scripts/hover_manager.gd
+++ scripts/hover_manager.gd
@@ -9,15 +9,18 @@ func _ready():
 	get_tree().root.call_deferred("add_child", label)
 
 func _process(_delta):
-	if label.visible:
-		label.global_position = get_viewport().get_mouse_position() + Vector2(16, -24)
+	#if label.visible:
+	#	label.global_position = get_viewport().get_mouse_position() + Vector2(16, -24)
+	pass
 
 func on_hovered(interactable):
 	current_hovered = interactable
-	label.text = interactable.object_name
-	print(interactable)
-	label.visible = true
+	#label.text = interactable.object_name
+	#print(interactable)
+	#label.visible = true
+	pass
 
 func on_unhovered(_interactable):
 	current_hovered = null
-	label.visible = false
+	#label.visible = false
+	pass
